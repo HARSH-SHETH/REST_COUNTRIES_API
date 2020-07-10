9491fdfe80ba9a4e1545859bd2a6b0ddea14b1c5
HARSH-SHETH <ycdtharsh@gmail.com>
2020-07-10 12:33:38 +0530

    initial page layout completed

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ window.onload = function(){
   request.onload = function(){
     var data = request.response;
     for(var i = 0; i < data.length; i++)
-      document.body.querySelector(".countries div").innerHTML += "<div><img src=\"" + data[i].flag + "\"></div>";
+      document.body.querySelector(".countries div").innerHTML +=
+    "<div><img src=\"" + data[i].flag + "\"><div class=\"info\">" +
+    "<h1>" + data[i].name + "</h1>" + 
+      "<p>Population: <span>" + 
+    Intl.NumberFormat().format(data[i].population) + 
+      "</span></p>" +
+      "<p>Region: <span>" + data[i].region + "</span></p>" +
+      "<p>Capital: <span>" + data[i].capital + "</span></p>"
+    "</div></div>";
   }
   request.send();
 }
